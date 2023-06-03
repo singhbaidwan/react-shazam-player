fab0902c1c93f2a50f6ffedf1b258ac9e8f6385a
Dalveer Singh <59986053+singhbaidwan@users.noreply.github.com>
2023-06-03 18:25:21 +0530

    added artist details , around you and top artists

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -1,3 +1,28 @@
-const TopArtists = () => <div>TopArtists</div>;
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { ArtistCard, Error, Loader, SongCard } from "../components";
+import { useGetSongsByCountryQuery } from "../redux/services/shazamCore";
 
+const TopArtists = () => {
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetSongsByCountryQuery();
+
+  if (isFetching) return <Loader title={"Loading Songs around you"} />;
+
+  if (error) return <Error />;
+
+  return (
+    <div className="flex flex-col ">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Discover Top Artists
+      </h2>
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.tracks.map((song, i) => (
+          <ArtistCard key={song.key} track={song} />
+        ))}
+      </div>
+    </div>
+  );
+};
 export default TopArtists;
--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,3 +1,35 @@
-const TopCharts = () => <div>TopCharts</div>;
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { Error, Loader, SongCard } from "../components";
+import { useGetSongsByCountryQuery } from "../redux/services/shazamCore";
 
+const TopCharts = () => {
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetSongsByCountryQuery();
+
+  if (isFetching) return <Loader title={"Loading Songs around you"} />;
+
+  if (error) return <Error />;
+
+  return (
+    <div className="flex flex-col ">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Discover Top Charts
+      </h2>
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.tracks.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 export default TopCharts;
